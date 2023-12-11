d6cdf50560e75770dcaa7afcc3a8c111d0496fbd
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2023-12-11 23:14:18 +0900

    fix Patch Tag

---
+++ Overlayer.State/TagPatch.cs
@@ -0,0 +1,12 @@
+﻿using Overlayer.Core.Patches;
+
+namespace State {
+    [LazyPatch("State.PatchTrigger", "TagManager", "UpdateReference", Triggers = new[] { "State" })]
+    public class TagPatch {
+        public static void Postfix() {
+            LazyPatchManager.PatchAll("StartTile");
+            LazyPatchManager.PatchAll("CurBpm");
+            LazyPatchManager.PatchAll("CurP");
+        }
+    }
+}
